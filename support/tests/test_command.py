8234e5dff9265d9fd5a94ffff5e58e154664395d
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-05-27 23:46:12 -0300

    Add a way to reload the tests from the main command file

--- support/tests/test_command.py
+++ support/tests/test_command.py
@@ -1,8 +1,13 @@
-import unittest
+#  Remember to install AAAPT package to run the tests
 
-# Remember: 
+import unittest
 #   Import your package here
-#   Install AAAPT package to run the tests
+
+# To reload your tests every time you save your command file, add the following to it:
+# for test_file in glob.glob("tests/test_*.py"):
+#     key = "{package_name}." + test_file[:-3].replace("/", ".")
+#     if key in sys.modules:
+#         reload(sys.modules[key])
 
 class Test_{package_name}Command(unittest.TestCase):
     pass
